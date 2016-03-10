75622863d0480c1624d1c2715a993715352c049e
Christian Fleschhut <weltrekordhalter@gmail.com>
2016-03-10 01:54:55 +0100

    create initial webpack setup + react component

---
+++ webpack.config.js
@@ -0,0 +1,32 @@
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+  entry: [
+    './app/index.js'
+  ],
+  module: {
+    loaders: [
+      {
+        test: /\.coffee$/,
+        exclude: /node_modules/,
+        loader: 'coffee-loader'
+      },
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader'
+      }
+    ]
+  },
+  output: {
+    filename: 'index_bundle.js',
+    path: __dirname  + '/dist'
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: __dirname + '/app/index.html',
+      filename: 'index.html',
+      inject: 'body'
+    })
+  ]
+};
